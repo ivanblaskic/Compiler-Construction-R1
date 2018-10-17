9f992d0ccc07cbd32ca7331c89344811eed33a15
ivanblaskic <39534048+ivanblaskic@users.noreply.github.com>
2018-10-17 12:55:38 -0400

    Add files via upload

--- R0Language.cpp
+++ R0Language.cpp
@@ -2,9 +2,13 @@
 
 int main() {
 
-	expR0 *program = new addR0(new negR0(new intR0(5)), new addR0(new readR0(new varR0("x")), new intR0(10)));
-	cout << program->eval() << "\n";
-	cout << program->toString() << "\n";
+	expR0 *program = new addR0(new negR0(new intR0(5)), new addR0(new readR0(), new intR0(10)));
+	cout << program->toString() << " = " << program->eval() << "\n\n";
+
+	expR0 *program2 = new letR0(new varR0("x"), new addR0(new intR0(5), new intR0(7)), new addR0(new varR0("x"), new intR0(5)));
+	cout << program2->toString() << " = " <<  program2->eval() << "\n";
+
 	system("pause");
 
 }
+
--- R0Language.hpp
+++ R0Language.hpp
@@ -1,6 +1,61 @@
 // (program (+ 5 (let(x (+ 5 6)) (+ x 2)))) 
-// add (int(5),let(x,add(5,6),add(x,2))) 
+// add (int(5),let(var(x),add(int(5),int(6)),add(var(x),int(2))))  --> add (int(5), let(var(x),int(11),add(var(x),int(2)))	--> list: (x, 11)  --> 
 // --> crucial step: add (int(5),add(add(5,6),2)) 
+/*
+
+	exp ::= int
+		  | (read)
+		  | (-exp)
+		  | (+ exp exp)
+		  | var
+		  | (let ([var exp]) exp)
+	R1  ::= (program exp)
+
+	register ::= rsp
+			   | rbp
+			   | rax
+			   | rbx
+			   | rcx
+			   | rdx
+			   | rsi
+			   | rdi
+			   | r8
+			   | r9
+			   | r10
+			   | r11
+			   | r12
+			   | r13
+			   | r14
+			   | r15
+	arg   ::= (int int)
+			| (reg register)
+			| (deref register int)
+	instr ::= (addq arg arg)
+			| (subq arg arg)
+			| (movq arg arg)
+			| (retq)
+			| (negq arg)
+			| (callq label)
+			| (pushq arg)
+			| (popq arg)
+	X860  ::= (program int instr+)
+
+	arg  ::= int | var
+	exp  ::= arg | (read) | (-arg)
+		   | (+ arg arg)
+	stmt ::= (assign var exp)
+		   | (return arg)
+	C0   ::= (program (var*) stmt+)
+
+	uniquify:
+		- structurally recursive f-ion
+		  translating input AST into an
+		  output AST - traversing entire
+		  AST
+
+	flatten
+
+*/
 // 
 
 #pragma once
@@ -12,11 +67,19 @@
 #include <string>
 #include <iostream>
 #include <list>
+#include <algorithm>
 
 using namespace std;
 
 static list<pair<std::string, int>> list_vars;
-static int counter;
+
+static int lookup(string var) {
+	std::list<pair<std::string, int>>::iterator it;
+	for (it = list_vars.begin(); it != list_vars.end(); ++it) {
+		if ((*it).first == var) return ((*it).second);
+	}
+	return 0;
+}
 
 class expR0 {
 
@@ -92,32 +155,62 @@ private:
 
 };
 
+class readR0 : public expR0 {
+
+public:
+	
+	readR0(void) { }
+	
+	int eval() {
+		cout << "Input the value for read: ";
+		cin >> this->val;
+		return this->val;
+	}
+	
+	string toString() {
+		return "(read)";
+	}
+
+private:
+	
+	int val;
+	
+};
+
 class varR0 : public expR0 {
 
 public:
 
 	varR0(string label) {
-		lab = label;
+		this->lab = label;
 	}
 
 	string toString() {
-		return "(" + this->lab + ")";
+		return this->lab;
 	}
 
 	void setVal(int value) {
 		this->val = value;
-		pair<std::string, int> temp; 
-		temp.first = lab;
-		temp.second = val;
-		list_vars.push_back(temp);
+		if (lookup(this->lab) == 0) {
+			std::pair<std::string, int> new_value = std::make_pair(this->lab, this->val);
+			list_vars.push_back(new_value);
+		}
+
+	}
+
+	int getVal() {
+		if (lookup(this->lab) != 0) {
+			return lookup(this->lab);
+		}
+		else {
+			cout << "Error trying to calculate with uninitialized variable: " << this->lab << ".";
+			return 0;
+		}
 	}
 
 	int eval() {
-		int temp;
-		cout << "Input the value for read: ";
-		cin >> temp;
-		setVal(temp);
-		return temp;
+		this->val = getVal();
+		return this->val;
 	}
 
 private:
@@ -125,7 +218,7 @@ private:
 	int val;
 };
 
-/*class letR0 : public expR0 {
+class letR0 : public expR0 {
 
 public:
 
@@ -137,33 +230,18 @@ public:
 
 	int eval() {
 		this->lab->setVal(this->vr_vl->eval());
-
-	}
-
-private:
-	varR0 *lab;
-	expR0 *vr_vl, *fn;
-};
-*/
-
-class readR0 : public expR0 {
-
-public:
-	
-	readR0(varR0* label) {
-		this->lab = label;
-	}
-
-	int eval() {
-		return this->lab->eval();
+		return this->fn->eval();					
 	}
 
 	string toString() {
-		return "(Read" + this->lab->toString() + ")";
+		return "(let (" + this->lab->toString() + " " + this->vr_vl->toString() + ")" + this->fn->toString() + ")";
 	}
 
 private:
+
 	varR0 *lab;
+	expR0 *vr_vl, *fn;
 };
+
 
 #endif
