f2fb4d9fd262946b85c9fa82f833e7e64ad349c2
ivanblaskic <39534048+ivanblaskic@users.noreply.github.com>
2018-11-09 09:41:15 -0500

    Add files via upload

--- R0Language.cpp
+++ R0Language.cpp
@@ -12,25 +12,49 @@
 int main() {
 
 	list<pair<std::string, int>> *code = new list<pair<std::string, int>>();
+	//list<pair<std::string, int>> *code1 = new list<pair<std::string, int>>();
 
+
+	/*
 	expR0 *program = new addR0(new negR0(new intR0(5)), 
 							   new addR0(new readR0(), 
 										 new intR0(10)));
 	cout << program->toString() << " = " << program->eval(code) << "\n\n";
+	*/
 
+	/*
+	expR0 *program1 = new addR0(new negR0(new intR0(5)),
+								new addR0(new readR0(),
+										  new intR0(10)));
+	*/
+
+	static list<pair<unique_ptr<varR0>, unique_ptr<varR0>>> *map_vars = new list<pair<unique_ptr<varR0>, unique_ptr<varR0>>>();
+	
+	/*
+	expR0 *program3 = program1->uniquify(map_vars);
+	cout << program3->toString() << " = " << program3->eval(code) << "\n\n";
+	*/
+
+	/*
 	expR0 *program2 = new letR0(new varR0("x"), 
 								new addR0(new intR0(5), 
 										  new intR0(7)), 
 								new addR0(new varR0("x"), 
 										  new intR0(5)));
 	cout << program2->toString() << " = " <<  program2->eval(code) << "\n\n";
-
-	expR0* program3 = new letR0(new varR0("x"), 
+	*/
+	
+	expR0* program4 = new letR0(new varR0("x"), 
 								new intR0(5),
 								new addR0(new letR0(new varR0("x"), 
 													new intR0(6), 
 													new varR0("x")), 
 										  new varR0("x")));
+	//cout << program4->toString() << " = " << program4->eval(code) << "\n\n";
+	
+	//system("pause");
+
+	expR0 *program3 = program4->uniquify(map_vars);
 	cout << program3->toString() << " = " << program3->eval(code) << "\n\n";
 
 	system("pause");
--- R0Language.hpp
+++ R0Language.hpp
@@ -27,8 +27,15 @@
 #include <iostream>
 #include <list>
 #include <algorithm>
+#include <memory>
 
 using namespace std;
+
+static int cnt = 0;
+
+
+class varR0;
+
 
 // EXP
 class expR0 {
@@ -37,8 +44,78 @@ public:
 
 	int virtual eval(list<pair<std::string, int>> *env) = 0;
 	virtual string toString() = 0;
+	virtual expR0* uniquify(list<pair<unique_ptr<varR0>, unique_ptr<varR0>>> *mapp) = 0;
 
 private:
+
+};
+
+
+// VAR ( string )
+class varR0 : public expR0 {
+
+public:
+
+	varR0(string label) {
+		this->lab = label;
+	}
+
+	int get_val(void) {
+		return this->val;
+	}
+
+	void setString(string temp) {
+		this->lab = temp;
+	}
+
+	varR0(varR0 *old_var) {
+		this->lab = old_var->lab + "_" + to_string(cnt++);
+		this->val = old_var->get_val();
+	}
+
+	string toString() {
+		return this->lab;
+	}
+
+	int eval(list<pair<std::string, int>> *env) {
+		std::list<pair<std::string, int>>::iterator it;
+		for (it = (*env).begin(); it != (*env).end(); ++it) {
+			if ((*it).first == this->lab) {
+				this->val = (*it).second;
+				return this->val;
+			}
+		}
+		cout << "- dealing with uninitialized variable -";
+		return 0;
+	}
+
+	string get_lab() {
+		return this->lab;
+	}
+
+	varR0* ret_val() {
+		return this;
+	}
+
+	virtual expR0* uniquify(list<pair<unique_ptr<varR0>, unique_ptr<varR0>>> *mapp) {
+		this->map = mapp;
+		std::list<pair<unique_ptr<varR0>, unique_ptr<varR0>>>::iterator it;
+		for (it = (*map).begin(); it != (*map).end(); ++it) {
+			if ((*it).first->toString() == this->toString()) {				
+				this->setString((*it).second->toString());
+				system("pause");
+				return this;
+			}
+		}
+		cout << "No mapping - error occured.";
+		return this;
+	}
+
+private:
+
+	list<pair<unique_ptr<varR0>, unique_ptr<varR0>>> *map;
+	string lab;
+	int val;
 
 };
 
@@ -60,6 +137,10 @@ public:
 		this->val = value;
 	}
 
+	virtual expR0* uniquify(list<pair<unique_ptr<varR0>, unique_ptr<varR0>>> *mapp) {
+		return this;
+	}
+
 private:
 
 	int val;
@@ -73,21 +154,27 @@ class addR0 : public expR0 {
 public:
 
 	int eval(list<pair<std::string, int>> *env) {
-		return (this->l->eval(env) + this->r->eval(env));
+		return (this->left->eval(env) + this->right->eval(env));
 	}
 
 	string toString() {
-		return "(+ " + this->l->toString() + " " + this->r->toString() + ")";
+		return "(+ " + this->left->toString() + " " + this->right->toString() + ")";
 	}
 
-	addR0(expR0* left, expR0* right) {
-		this->l = left;
-		this->r = right;
+	addR0(expR0* left_, expR0* right_) {
+		this->left = left_;
+		this->right = right_;
+	}
+
+	virtual expR0* uniquify(list<pair<unique_ptr<varR0>, unique_ptr<varR0>>> *mapp) {
+		map = mapp;
+		return new addR0(this->left->uniquify(map), this->right->uniquify(map));
 	}
 
 private:
 
-	expR0 *l, *r;
+	expR0 *left, *right;
+	list<pair<unique_ptr<varR0>, unique_ptr<varR0>>> *map;
 
 };
 
@@ -109,9 +196,15 @@ public:
 		this->val = value;
 	}
 
+	virtual expR0* uniquify(list<pair<unique_ptr<varR0>, unique_ptr<varR0>>> *mapp) {
+		map = mapp;
+		return new negR0(this->val->uniquify(map));
+	}
+
 private:
 
 	expR0 *val;
+	list<pair<unique_ptr<varR0>, unique_ptr<varR0>>> *map;
 
 };
 
@@ -133,40 +226,14 @@ public:
 		return "(read)";
 	}
 
+	virtual expR0* uniquify(list<pair<unique_ptr<varR0>, unique_ptr<varR0>>> *mapp) {
+		return this;
+	}
+
 private:
 	
 	int val;
 	
-};
-
-
-// VAR ( string )
-class varR0 : public expR0 {
-
-public:
-
-	varR0(string label) {
-		this->lab = label;
-	}
-
-	string toString() {
-		return this->lab;
-	}
-
-	int eval(list<pair<std::string, int>> *env) {
-		std::list<pair<std::string, int>>::iterator it;
-		for (it = (*env).begin(); it != (*env).end(); ++it) {
-			if ((*it).first == this->lab) return ((*it).second);
-		}
-		cout << "- dealing with uninitialized variable -";
-		return 0;
-	}
-
-private:
-
-	string lab;
-	int val;
-
 };
 
 
@@ -191,18 +258,43 @@ public:
 				return fn->eval(new_env);
 			}
 		}
-		(*new_env).push_back(std::make_pair(this->lab->toString(), this->vr_vl->eval(env)));
-		return fn->eval(new_env);		
+		int temp = this->vr_vl->eval(env);
+		(*env).push_back(std::make_pair(this->lab->toString(), temp));
+		return fn->eval(env);
 	}
 
 	string toString() {
 		return "(let [" + this->lab->toString() + " " + this->vr_vl->toString() + "]" + this->fn->toString() + ")";
 	}
 
-private:
+	virtual expR0* uniquify(list<pair<unique_ptr<varR0>, unique_ptr<varR0>>> *mapp) {
+		this->map = mapp;
+		varR0 *new_variable = new varR0(this->lab);										// variable with label
+		std::list<pair<unique_ptr<varR0>, unique_ptr<varR0>>>::iterator it;				// let's see if we have one with the same init name
+		for (it = (*map).begin(); it != (*map).end(); ++it) { 
+			if ((*it).first->toString() == this->lab->toString()) {						// if we do
+				this->new_map = new list<pair<unique_ptr<varR0>, unique_ptr<varR0>>>(); // create new mapping list
+				new_map = map;															// same as the one before 
+				std::list<pair<unique_ptr<varR0>, unique_ptr<varR0>>>::iterator it1;	// find the same named var
+				for (it1 = (*new_map).begin(); it1 != (*new_map).end(); ++it1) {		
+					if ((*it1).first->toString() == this->lab->toString()) {
+						(*it1).second = unique_ptr<varR0>(new_variable);				// change it's matching var to new_variable
+					}
+				}
+				return new letR0(new_variable, this->vr_vl->uniquify(new_map), this->fn->uniquify(new_map));	//return let with new mapping
+			}
+		}																						// if that's not the case enter pair in mapping
+		(*map).emplace_back(std::make_pair(unique_ptr<varR0>(new varR0(this->lab->toString())), unique_ptr<varR0>(new_variable)));
+		return new letR0(new_variable, this->vr_vl->uniquify(map), this->fn->uniquify(map));	// return regular mapping with let
+		this->lab = new_variable;
+	}
 
+private:
+	
+	list<pair<unique_ptr<varR0>, unique_ptr<varR0>>> *new_map;
 	varR0 *lab;
 	expR0 *vr_vl, *fn;
+	list<pair<unique_ptr<varR0>, unique_ptr<varR0>>> *map;
 
 };
 
